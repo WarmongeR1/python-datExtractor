315adfb514dc6e6949eea57276ef16cedc874c0e
Alexander Sapronov <sapronov.alexander92@gmail.com>
2015-09-18 11:07:00 +0600

    ADD: top sites (res file)

---
+++ resources/top_sites/__init__.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+
+all = [(1, 'google.com'), (2, 'facebook.com'), (3, 'youtube.com'), (4, 'baidu.com'), (5, 'yahoo.com'), (6, 'amazon.com'), (7, 'wikipedia.org'), (8, 'qq.com'), (9, 'twitter.com'), (10, 'google.co.in'), (11, 'taobao.com'), (12, 'live.com'), (13, 'sina.com.cn'), (14, 'linkedin.com'), (15, 'yahoo.co.jp'), (16, 'weibo.com'), (17, 'ebay.com'), (18, 'google.co.jp'), (19, 'yandex.ru'), (20, 'vk.com'), (21, 'hao123.com'), (22, 'google.de'), (23, 't.co'), (24, 'bing.com'), (25, 'blogspot.com'), (26, 'aliexpress.com'), (27, 'instagram.com'), (28, 'msn.com'), (29, 'amazon.co.jp'), (30, 'google.co.uk'), (31, 'ask.com'), (32, 'google.com.br'), (33, 'pinterest.com'), (34, 'tmall.com'), (35, 'reddit.com'), (36, 'wordpress.com'), (37, 'google.fr'), (38, 'mail.ru'), (39, 'paypal.com'), (40, 'apple.com'), (41, 'onclickads.net'), (42, 'microsoft.com'), (43, 'sohu.com'), (44, 'tumblr.com'), (45, 'google.ru'), (46, 'imgur.com'), (47, 'xvideos.com'), (48, 'google.it'), (49, 'imdb.com'), (50, 'google.es'), (51, 'fc2.com'), (52, 'googleadservices.com'), (53, 'netflix.com'), (54, 'amazon.de'), (55, 'gmw.cn'), (56, '360.cn'), (57, 'stackoverflow.com'), (58, 'alibaba.com'), (59, 'tianya.cn'), (60, 'craigslist.org'), (61, 'google.com.mx'), (62, 'ok.ru'), (63, 'google.ca'), (64, 'go.com'), (65, 'google.com.hk'), (66, 'naver.com'), (67, 'pornhub.com'), (68, 'amazon.co.uk'), (69, 'rakuten.co.jp'), (70, 'diply.com'), (71, 'blogger.com'), (72, 'xhamster.com'), (73, 'google.com.tr'), (74, 'cnn.com'), (75, 'outbrain.com'), (76, 'kat.cr'), (77, 'soso.com'), (78, 'adcash.com'), (79, 'xinhuanet.com'), (80, 'nicovideo.jp'), (81, 'espn.go.com'), (82, 'cntv.cn'), (83, 'bbc.co.uk'), (84, 'flipkart.com'), (85, 'google.co.id'), (86, 'googleusercontent.com'), (87, 'github.com'), (88, 'dropbox.com'), (89, 'booking.com'), (90, 'amazon.in'), (91, 'people.com.cn'), (92, 'google.co.kr'), (93, 'pixnet.net'), (94, 'google.com.au'), (95, 'google.pl'), (96, 'ebay.de'), (97, 'popads.net'), (98, '163.com'), (99, 'ebay.co.uk'), (100, 'bbc.com')]
+russian = [
+    'Livejournal.com',
+    'Liveinternet.ru',
+    'rbc.ru'
+    'kinopoisk.ru'
+    'Lenta.ru',
+    'hh.ru',
+    'drom.ru',
+    'ria.ru',
+    'auto.ru',
+    'blogspot.ru',
+    'Drive2.ru',
+    'Gazeta.ru',
+    'Adme.ru',
+    'Fotostrana.ru',
+    'Fishki.net',
+    'Habrahabr.ru',
+    'Vesti.ru',
+    'Pikabu.ru',
+]
